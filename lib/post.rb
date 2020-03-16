df53f51d38d6d89196e650aa0c1ac37293c4e840
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-16 19:07:37 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,11 @@
-class Post
+class Posts
   
 attr_accessor :title, :author 
 
   def initialize (title, author)
     @title = title 
     @author = author 
+    Author.Posts << self 
   end 
   
 end 
